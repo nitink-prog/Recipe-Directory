3f180b35a591e3e22b1029608c1806f0200ea82e
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-17 16:28:53 -0400

    working on Add New Recipe form

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,5 +1,61 @@
+import { useState } from "react";
 import "./Create.css";
 
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const [newIngredient, setNewIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe ingredients:</span>
+          <div>
+            <input type="text" />
+            <button className="btn">Add</button>
+          </div>
+        </label>
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
 }
