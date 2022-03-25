dec4af8645c32e3f5314a2fccadd7feec86bdf10
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-25 10:22:07 -0400

    beginning transition to firebase backend

---
+++ src/firebase/config.jsx
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWsOwZE2zjugELlZyTnWdkQzKmY0xJFYE",
+  authDomain: "recipe-directory-86243.firebaseapp.com",
+  projectId: "recipe-directory-86243",
+  storageBucket: "recipe-directory-86243.appspot.com",
+  messagingSenderId: "394050848754",
+  appId: "1:394050848754:web:0f8b8091099b2918154d6b",
+};
+
+// connect to firebase backend
+firebase.initializeApp(firebaseConfig);
+
+// initialize firestore database service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
