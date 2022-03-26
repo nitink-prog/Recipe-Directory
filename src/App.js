0e9b8f5cb9f6b409cf60b14a51d1dfddd85fc056
Nitin K <80429955+nitink-prog@users.noreply.github.com>
2022-03-26 10:39:50 -0400

    swapped imports for readability

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useTheme } from "./hooks/useTheme";
 import Navbar from "./components/Navbar";
+import ThemeSelector from "./components/ThemeSelector";
 import Create from "./pages/create/Create";
 import Home from "./pages/home/Home";
 import Recipe from "./pages/recipe/Recipe";
 import Search from "./pages/search/Search";
 import "./App.css";
-import ThemeSelector from "./components/ThemeSelector";
-import { useTheme } from "./hooks/useTheme";
 
 function App() {
   const { mode } = useTheme();
