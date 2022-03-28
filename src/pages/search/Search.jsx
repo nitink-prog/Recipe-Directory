61bfafee9143b6c064b92ff5b4cb9a6dff6f55dc
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-28 13:04:26 -0400

    Firestore search works now... sort of. read desc.
    Firestore doesn't have built in string querying which I find to be a major downside to the platform - Devs would need to add Algolia or other extension which is not free. My search implementation here is decent, and it works. It is case-sensitive. "Veg" will return "Vegetable Stew" but "veg" will not return it.

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,33 +1,49 @@
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { useTheme } from "../../hooks/useTheme";
 import RecipeList from "../../components/RecipeList";
 import { db } from "../../firebase/config";
 import "./Search.css";
 
 export default function Search() {
+  const [recipes, setRecipes] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  const { mode } = useTheme();
+
   // get searchQuery out of the URL
   const queryString = useLocation();
-  const searchQuery = queryString.search.substring(3);
+  const trimString = queryString.search.substring(3);
+  const searchQuery = trimString.replace("%20", " ");
 
   // create query object for Firestore
-  const q = db
-    .collection("recipes")
-    .where("title", "in", searchQuery)
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(doc.data());
+  useEffect(() => {
+    setIsPending(true);
+    const q = db
+      .collection("recipes")
+      .where("title", ">=", searchQuery)
+      .get()
+      .then((querySnapshot) => {
+        let results = [];
+        querySnapshot.forEach((doc) => {
+          results.push({ id: doc.id, ...doc.data() });
+        });
+        setRecipes(results);
+        setIsPending(false);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err);
       });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  }, [searchQuery]);
 
   return (
-    <div>
-      <h2 className="page-title">
-        Recipes including "{queryString.search.substring(3)}"
-      </h2>
-      {/* {data && <RecipeList recipes={data} />} */}
+    <div className={`search ${mode}`}>
+      <h2 className="page-title">Recipes including "{searchQuery}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes && <RecipeList recipes={recipes} />}
     </div>
   );
 }
