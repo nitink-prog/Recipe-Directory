e3b3f1be6c1b77b9f73bae1e103d43049717fe3c
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-28 12:59:57 -0400

    useState first, then useEffect...
    just something that was bothering me

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import RecipeList from "../../components/RecipeList";
 import { db } from "../../firebase/config";
 import "./Home.css";
